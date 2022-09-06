854235eab1f9b077222cb7453979e0848f2869a2
Alfa2337 <azizaliev2337@gmail.com>
2022-09-06 19:54:38 +0500

    add docs swagger

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,6 +7,21 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+/**
+ * @SWG\SWagger(
+ *   @OA\Info(
+ *      title="Documention api laravel with swagger",
+ *      description="Example",
+ *      version="1.0.0"
+ *   )
+ *   @OA\SecurityScheme(
+ *      securityScheme="bearerAuth",
+ *      type="http",
+ *      scheme="bearer",
+ *      in="header"
+ *   )
+ * )
+ */
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
