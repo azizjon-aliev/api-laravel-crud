38588c98ea1ff6e178ad90a9d3fd4e8465c6f61c
Alfa2337 <azizaliev2337@gmail.com>
2022-09-07 12:23:31 +0500

    edit routes post

--- routes/v1/api.php
+++ routes/v1/api.php
@@ -1,7 +1,5 @@
 <?php
 
-// use App\Http\Controllers\Api\V1\AuthController;
-
 use App\Http\Controllers\Api\V1\AuthController;
 use Illuminate\Support\Facades\Route;
 
@@ -11,15 +9,7 @@ Route::namespace('App\Http\Controllers\Api\V1')->group(function() {
     Route::post('/register', [AuthController::class, 'register']);
 
     Route::middleware('auth:api')->group(function () {
-
         Route::apiResource('category', CategoryController::class);
-
-        Route::prefix('post')->namespace('Post')->group(function() {
-            Route::get('/', IndexController::class);
-            Route::get('/{post}', ShowController::class);
-            Route::post('/', StoreController::class);
-            Route::put('/{post}', UpdateController::class);
-            Route::delete('/{post}', DestroyController::class);
-        });
+        Route::apiResource('post', PostController::class);
     });
 });
